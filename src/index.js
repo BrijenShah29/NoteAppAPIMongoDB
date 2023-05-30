a604cae9cef0cc29ba1e128b630e8cc6322d8927
Brijen <bshah91@my.centennialcollege.ca>
2023-05-29 21:14:24 -0400

    Notes API CRUD Operations

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 const express = require("express");
+const userRouter = require("./routes/userRoutes");
+const noteRouter = require("./routes/noteRoutes");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
+
+const mongoose = require("mongoose");
+
+app.use(express.json());
+
+app.use("/users",userRouter);
+
+app.use("/note",noteRouter);
+
 
 app.get("/",(req, res)=>{
     res.send("Hello");
 });
 
-app.get("/quote",(req,res)=>{
-    res.send("Quote");
-});
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    app.listen(5000,()=>{
+        console.log("Server started on port no. 5000");
+    });
+})
+.catch((error)=>{
+    console.log(error);
+})
 
-app.listen(5000,()=>{
-    console.log("Server started on port no. 5000");
-});
+
+
 
